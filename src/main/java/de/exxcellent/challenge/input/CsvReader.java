d0e4b785e2c7dbe57c87bf5b7913db4aaa2971ba
Matze <mfriedri@stud.hs-heilbronn.de>
2019-01-31 17:19:49 +0100

    refactord Reader Interface to return List of Strings

--- src/main/java/de/exxcellent/challenge/input/CsvReader.java
+++ src/main/java/de/exxcellent/challenge/input/CsvReader.java
@@ -2,13 +2,17 @@ package main.java.de.exxcellent.challenge.input;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class CsvReader implements Reader{
 
 
 
     @Override
-    public Map<Integer, List<String>> getData() {
+    public Map<Integer, List<String>> getData(String filePath) {
+        Scanner imputStram = new Scanner(filePath);
+
+
         return null;
     }
 }
--- src/main/java/de/exxcellent/challenge/input/Reader.java
+++ src/main/java/de/exxcellent/challenge/input/Reader.java
@@ -8,7 +8,7 @@ public interface Reader {
     /**
      * get the data from the File ignoring the headers header cow
      * @param filePath path of the file to read
-     * @return am map with key: row number, value: colum values in a List
+     * @return a List of the values of the colums
      */
-    public Map<Integer, List<String>> getData(String filePath);
+    public List<String> getData(String filePath);
 }
