099a8c37f497b0887bff4be6c4352c62e9d35e06
Matze <mfriedri@stud.hs-heilbronn.de>
2019-01-31 17:03:17 +0100

    added filpath parameter in Reader Interface

--- src/main/java/de/exxcellent/challenge/input/Reader.java
+++ src/main/java/de/exxcellent/challenge/input/Reader.java
@@ -7,7 +7,8 @@ public interface Reader {
 
     /**
      * get the data from the File ignoring the headers header cow
+     * @param filePath path of the file to read
      * @return am map with key: row number, value: colum values in a List
      */
-    public Map<Integer, List<String>> getData();
+    public Map<Integer, List<String>> getData(String filePath);
 }
