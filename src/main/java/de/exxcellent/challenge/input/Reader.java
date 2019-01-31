d14c32ca3bdce27006079263c8dca5bb4a9f9b3f
Matze <mfriedri@stud.hs-heilbronn.de>
2019-01-31 16:50:11 +0100

    added Reader Interface

---
+++ src/main/java/de/exxcellent/challenge/input/Reader.java
@@ -0,0 +1,13 @@
+package main.java.de.exxcellent.challenge.input;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Reader {
+
+    /**
+     * get the data from the File
+     * @return am map with key: row number, value: colum values in a List
+     */
+    public Map<Integer, List<String>> getData();
+}
