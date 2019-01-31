ef6b1145f0b10e72ac1f913bb3395415f8dd0fde
Matze <mfriedri@stud.hs-heilbronn.de>
2019-01-31 21:34:20 +0100

    added FoorballdataHandler and SpreadCalculator classes

---
+++ src/main/java/de/exxcellent/challenge/Logic/FoorballdataHandler.java
@@ -0,0 +1,6 @@
+package main.java.de.exxcellent.challenge.Logic;
+
+public class FoorballdataHandler {
+
+
+}
---
+++ src/main/java/de/exxcellent/challenge/Logic/SpreadCalculator.java
@@ -0,0 +1,4 @@
+package main.java.de.exxcellent.challenge.Logic;
+
+public class SpreadCalculator {
+}
