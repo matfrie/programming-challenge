df41aa5aa295c9737d0697af4c89f15c393c322d
Matze <mfriedri@stud.hs-heilbronn.de>
2019-01-31 16:58:15 +0100

    added CsvReader class

---
+++ src/main/java/de/exxcellent/challenge/input/CsvReader.java
@@ -0,0 +1,14 @@
+package main.java.de.exxcellent.challenge.input;
+
+import java.util.List;
+import java.util.Map;
+
+public class CsvReader implements Reader{
+
+
+
+    @Override
+    public Map<Integer, List<String>> getData() {
+        return null;
+    }
+}
--- src/main/java/de/exxcellent/challenge/input/Reader.java
+++ src/main/java/de/exxcellent/challenge/input/Reader.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public interface Reader {
 
     /**
-     * get the data from the File
+     * get the data from the File ignoring the headers header cow
      * @return am map with key: row number, value: colum values in a List
      */
     public Map<Integer, List<String>> getData();
